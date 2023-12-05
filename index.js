5165bbdc0f9632e2ef0fe60fa29687d4aec6e93d
Roychh <shootheat2023@gmail.com>
2023-12-05 20:56:36 +0300

    cold btn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-process.setMaxListeners(15);
+process.setMaxListeners(20);
 const { Telegraf, Markup } = require('telegraf');
 require('dotenv').config()
 const text = require ('./const')
@@ -25,25 +25,56 @@ bot.command('hot_drinks', async (ctx)=> {
      }
 })
 
- bot.command('cold_drinks', async (ctx)=> {
+bot.command('cold_drinks', async (ctx) => {
      try {
-      await ctx.replyWithHTML('<b>ХОЛОДНЫЕ НАПИТКИ</b>', Markup.inlineKeyboard(
-           [
-                [Markup.button.callback('АЙС Американо','btn_21'), Markup.button.callback('Айс Латте','btn_22'), Markup.button.callback('Айс Матча','btn_23')],
-                [Markup.button.callback('Айс Раф','btn_24'), Markup.button.callback('Фрапучино Шоколадный','btn_25'), Markup.button.callback('Хорнет','btn_26')],
-                [Markup.button.callback('Эспрессо-тоник','btn_27'), Markup.button.callback('Банан Крем Кофе','btn_28'), Markup.button.callback('Айс Какао','btn_29')],
-                [Markup.button.callback('Сорбетто Клубника','btn_30'), Markup.button.callback('Сорбетто Ананас/манго/Вишня','btn_31'), Markup.button.callback('Крем-сода','btn_32')],
-                [Markup.button.callback('Лимонад Классический','btn_33'), Markup.button.callback('ФризБриз','btn_34'), Markup.button.callback('Лимонады ягодные','btn_35')],
-                [Markup.button.callback('Мохито','btn_36'), Markup.button.callback('Милк-шейки','btn_37'), Markup.button.callback('Смузи Сникерс/Орео','btn_38')],
-                [Markup.button.callback('Смузи Ягодные','btn_39'), Markup.button.callback('Смузи на растительном','btn_40'), Markup.button.callback('Фрапучино Сникерс/Орео','btn_41')],
-                [Markup.button.callback('Фрапучино Ваниль/Карамель/Фисташка','btn_42')],
-           ]
-      ))
-      } catch (e) {
-           console.error(e)
-      }
- })
-
+         await ctx.replyWithHTML('<b>ХОЛОДНЫЕ НАПИТКИ</b>', {
+             reply_markup: {
+                 inline_keyboard: [
+                     [
+                         { text: 'АЙС Американо', callback_data: 'btn_21' },
+                         { text: 'Айс Латте', callback_data: 'btn_22' },
+                         { text: 'Айс Матча', callback_data: 'btn_23' },
+                     ],
+                     [
+                         { text: 'Айс Раф', callback_data: 'btn_24' },
+                         { text: 'Фрапучино Шоколадный', callback_data: 'btn_25' },
+                         { text: 'Хорнет', callback_data: 'btn_26' },
+                     ],
+                     [
+                         { text: 'Эспрессо-тоник', callback_data: 'btn_27' },
+                         { text: 'Банан Крем Кофе', callback_data: 'btn_28' },
+                         { text: 'Айс Какао', callback_data: 'btn_29' },
+                     ],
+                     [
+                         { text: 'Сорбетто Клубника', callback_data: 'btn_30' },
+                         { text: 'Сорбетто Ананас/манго/Вишня', callback_data: 'btn_31' },
+                         { text: 'Крем-сода', callback_data: 'btn_32' },
+                     ],
+                     [
+                         { text: 'Лимонад Классический', callback_data: 'btn_33' },
+                         { text: 'ФризБриз', callback_data: 'btn_34' },
+                         { text: 'Лимонады ягодные', callback_data: 'btn_35' },
+                     ],
+                     [
+                         { text: 'Мохито', callback_data: 'btn_36' },
+                         { text: 'Милк-шейки', callback_data: 'btn_37' },
+                         { text: 'Смузи Сникерс/Орео', callback_data: 'btn_38' },
+                     ],
+                     [
+                         { text: 'Смузи Ягодные', callback_data: 'btn_39' },
+                         { text: 'Смузи на растительном', callback_data: 'btn_40' },
+                         { text: 'Фрапучино Сникерс/Орео', callback_data: 'btn_41' },
+                     ],
+                     [
+                         { text: 'Фрапучино Ваниль/Карамель/Фисташка', callback_data: 'btn_42' },
+                     ],
+                 ],
+             },
+         });
+     } catch (e) {
+         console.error(e);
+     }
+ });
  bot.command('showcase', async (ctx) => {
      try {
          await ctx.replyWithHTML('<b>Витрина</b>', {

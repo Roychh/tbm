52dbded18111d01ef04d3fe287de41dbb9a512a0
Roychh <shootheat2023@gmail.com>
2023-12-10 23:44:00 +0300

    в

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ process.setMaxListeners(20);
 const { Telegraf, Markup } = require('telegraf');
 require('dotenv').config()
 const text = require ('./const')
-
 const allowedPhoneNumbers = ['+79787040822']; // Замените на реальные номера телефонов
+
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply(`Привет ${ctx.message.from.first_name ? ctx.message.from.first_name : незнакомец}`))
 bot.help((ctx) => ctx.reply('text.commands'))
